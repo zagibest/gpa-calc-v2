e64230d4d58a38f7262f4b2b4ffc403a6079b0fc
Zandan-Ochir Ganbold <zagig35@gmail.com>
2023-01-28 00:32:01 +0800

    no nav

---
+++ dist/assets/NotFound.b75df324.js
@@ -0,0 +1,1 @@
+import{t as o}from"./vendor.c42405e9.js";const e=o("<main>Not found</main>");export default()=>e.cloneNode(!0);
--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ const routes = [
 const App: Component = () => {
   return (
     <Router routes={routes}>
-      <Nav />
       <Route />
     </Router>
   );
